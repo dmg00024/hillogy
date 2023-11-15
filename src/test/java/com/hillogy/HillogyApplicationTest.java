bd0aeb575c06c0125bd8316cd5e66f72c786088f
Daniel Muñoz Gallardo <dmg00024@gmail.com>
2023-11-15 21:09:05 +0100

    añadido el test de integración de arranque

--- src/test/java/com/hillogy/HillogyApplicationTest.java
+++ src/test/java/com/hillogy/HillogyApplicationTest.java
@@ -8,7 +8,13 @@ public class HillogyApplicationTest {
 
 	@Test
 	public void contextLoads() {
+	}
 
+	@Test
+	void mainMethodRunsSuccessfully() {
+		// Este método prueba la ejecución exitosa del método main
+		HillogyApplication.main(new String[] {}); // Puedes pasar los argumentos necesarios aquí
+		// Si la ejecución llega a este punto sin excepciones, se considera un éxito
 	}
 
 }
