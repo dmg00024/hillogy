b9b71661900827c6ce9f939d9988671fb1dbf050
Daniel Muñoz Gallardo <dmg00024@gmail.com>
2023-11-15 19:32:45 +0100

    test with Junit 4

--- src/test/java/com/hillogy/model/BookTest.java
+++ src/test/java/com/hillogy/model/BookTest.java
@@ -1,10 +1,11 @@
 package com.hillogy.model;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
+import org.junit.Test;
 
 public class BookTest {
 
