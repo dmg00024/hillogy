890f9b87ee5d8941bd155436c034c67bb51d12bc
Daniel Muñoz Gallardo <dmg00024@gmail.com>
2023-11-19 04:14:50 +0100

    swagger

--- src/main/java/com/hillogy/controller/LibraryController.java
+++ src/main/java/com/hillogy/controller/LibraryController.java
@@ -4,18 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hillogy.dto.BookDTO;
+import com.hillogy.exception.BookAlreadyExistsException;
 import com.hillogy.service.LibraryService;
 
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.Operation;
+
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 
 @Tag(name = "Library Management endpoints", description = "Library Management endpoints")
 @RestController
@@ -28,18 +31,30 @@ public class LibraryController {
 	/**
 	 * Adds a new book to the library.
 	 *
-	 * @param bookDTO The data transfer object containing the details of the book to be added.
-	 * @return A ResponseEntity with a boolean indicating whether the book was added successfully.
+	 * @param bookDTO the BookDTO containing the data of the new book.
+	 * @return ResponseEntity with status CREATED if the book was added
+	 *         successfully, BAD_REQUEST if the BookDTO is null or its fields are
+	 *         empty, CONFLICT if a book with the same ISBN already exists,
+	 *         INTERNAL_SERVER_ERROR for any other exceptions.
 	 */
-	@ApiOperation(value = "Add a new book to the library", response = Boolean.class)
-	@ApiResponses(value = {
-	    @ApiResponse(code = 201, message = "Book successfully added"),
-	    @ApiResponse(code = 400, message = "Invalid input"),
-	    @ApiResponse(code = 409, message = "Book already exists")
-	})
-	@PostMapping("/books")
-	public ResponseEntity<Boolean> addBook(@RequestBody @ApiParam(value = "Book data to add") BookDTO bookDTO) {
-	    boolean isAdded = libraryService.addBook(bookDTO);
-	    return new ResponseEntity<>(isAdded, HttpStatus.CREATED);
+	@Operation(summary = "Add a new book to the library")
+	@ApiResponses(value = { 
+			@ApiResponse(responseCode = "201", description = "Book added successfully", content = @Content),
+			@ApiResponse(responseCode = "400", description = "Invalid BookDTO supplied", content = @Content),
+			@ApiResponse(responseCode = "409", description = "Book already exists", content = @Content),
+			@ApiResponse(responseCode = "500", description = "Server error", content = @Content) })
+	@PostMapping(value = "/book", consumes = "application/json")
+	public ResponseEntity<?> addBook(@RequestBody BookDTO bookDTO) {
+		if (bookDTO == null || bookDTO.getIsbn() == null || bookDTO.getTitle() == null || bookDTO.getAuthor() == null) {
+			return new ResponseEntity<>("Invalid data input", HttpStatus.BAD_REQUEST);
+		}
+		try {
+			libraryService.addBook(bookDTO);
+			return new ResponseEntity<>("Book added successfully", HttpStatus.CREATED);
+		} catch (BookAlreadyExistsException e) {
+			return new ResponseEntity<>("Book already exists", HttpStatus.CONFLICT);
+		} catch (Exception e) {
+			return new ResponseEntity<>("Server error", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 }
