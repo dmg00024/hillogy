2332a1644d5b8465f68f2d2dd1c8d26ab93d3ac2
Daniel Muñoz Gallardo <dmg00024@gmail.com>
2023-11-20 12:06:25 +0100

    fix method not used in this exception

--- src/main/java/com/hillogy/exception/UserAlreadyExistsException.java
+++ src/main/java/com/hillogy/exception/UserAlreadyExistsException.java
@@ -10,8 +10,4 @@ public class UserAlreadyExistsException extends RuntimeException {
 	public UserAlreadyExistsException(String message) {
         super(message);
     }
-
-    public UserAlreadyExistsException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
